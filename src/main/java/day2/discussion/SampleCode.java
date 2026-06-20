e2788239b42a400aa9c3dc04ad2a5ac47af48b92
xyro <kentfloyd02@gmail.com>
2026-06-20 15:34:27 +0800

    Add discussion code for day2 and day3

---
+++ src/main/java/day2/discussion/SampleCode.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day2.discussion;
+
+/**
+ *
+ * @author ktagl
+ */
+public class SampleCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+// MAIN METHOD (psvm + tab)
+// Prin Statement (sout + tab)
+
+/* 2 Types off Comments
+
+1. simple line comment
+2. multi line comment
+
+*/
---
+++ src/main/java/day3/discussion/SampleCode.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day3.discussion;
+
+/**
+ *
+ * @author ktagl
+ */
+public class SampleCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+//Syntax -> structure of code 
+//Syntax rule
+
+
+// MAIN METHOD (psvm + tab)
+// Prin Statement (sout + tab)
+
+/* 2 Types off Comments
+
+1. simple line comment
+2. multi line comment
+
+*/
