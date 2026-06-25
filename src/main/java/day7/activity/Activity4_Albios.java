cfc4110580fd83df9c4243ffa11df39813eeb038
xyro <kentfloyd02@gmail.com>
2026-06-25 17:23:01 +0800

    code day 7

--- src/main/java/day7/activity/Activity4_Albios.java
+++ src/main/java/day7/activity/Activity4_Albios.java
@@ -7,6 +7,26 @@ import java.util.Scanner;
 
 public class Activity4_Albios {
     public static void main(String[] args) {
-        
+        Scanner input =new Scanner(System.in);
+  
+        System.out.println("== Kent Dev ==");
+        System.out.println("1. Start Game");
+        System.out.println("2. Load Game");
+        System.out.println("3. Setting");
+        System.out.println("4. Exit");
+        System.out.print("Enter your choice (1-4): ");
+        int choice = input.nextInt();
+
+        if(choice == 1){
+            System.out.print("You selected Option 1: Start Game");
+        }else if(choice == 2) {
+            System.out.print("You selected Option 2: Load Game");  
+        }else if(choice == 3) {
+            System.out.print("You selected Option 3: Settings");  
+        }else if(choice == 4) {
+            System.out.print("You selected Option 4: Exit");  
+        }else{
+            System.out.println("Invalid choice. Please try again.");
+        }
     }
 }
